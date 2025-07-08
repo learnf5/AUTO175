74e3486e1efbba07d7d6d22cbebb6284fd83efd1
learnf5 <99698691+learnf5@users.noreply.github.com>
2025-07-08 16:12:57 -0400

    Update 10-AUTO175.sh

--- 10-AUTO175.sh
+++ 10-AUTO175.sh
@@ -11,12 +11,18 @@ until dig f5.com | grep ^f5.com; do sleep 1; done
 # get file that will show which, if any, bigips are needed
 curl --silent https://raw.githubusercontent.com/learnf5/auto/main/v17.1/dev-roster.md --output /tmp/dev-roster.md
 
+# determine if bigip1 and bigip2 are used in this lab profile
+bip1=$(curl --silent https://raw.githubusercontent.com/learnf5/AUTO175/main/README.md | 
+  awk -F\| -vlab=$LAB_NUMBER 'BEGIN {bip1 = "absent"} /Lab VM/,/Lab Name/ {gsub(/ /, "", $2); gsub(//, "", $3); if ($2 == lab && $3 != "") bip1 = "present"} END {print bip1}')
+[[ $bip1 == present ]] && bip2=$(curl --silent https://raw.githubusercontent.com/learnf5/AUTO175/main/README.md | 
+  awk -F\| -vlab=$LAB_NUMBER 'BEGIN {bip2 = "absent"} /Lab VM/,/Lab Name/ {gsub(/ /, "", $2); gsub(//, "", $3); if ($2 == lab && $4 != "") bip2 = "present"} END {print bip2}')
+
 # change bigip1's hostname to bigip1a, if bigip1a is used
-###if grep "$lab_id.*bigip1a" /tmp/dev-roster.md; then
+if [[ $bip1 == present ]]; then
     until $(ssh-keyscan bigip1 >/dev/null 2>&1); do sleep 1; done    # wait until bigip2 is reachable
     ssh-keyscan bigip1 >>~/.ssh/known_hosts
     sshpass -p f5trn001 ssh root@bigip1 "tmsh modify sys global-settings hostname bigip1a.f5trn.com"
-###fi
+fi
 
 # download config from github, copy and load/merge it to bigip1b, if bigip1b is used
 ###if grep "$lab_id.*bigip1b" /tmp/dev-roster.md; then
