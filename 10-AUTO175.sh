92d6611fad5e9e6688a3d0ad29f6c32e70d233e5
learnf5 <99698691+learnf5@users.noreply.github.com>
2025-07-13 14:32:30 -0400

    Update 10-AUTO175.sh

--- 10-AUTO175.sh
+++ 10-AUTO175.sh
@@ -1,3 +1,17 @@
+function vm_present() {
+  vm=$1
+  
+  curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/README.md | 
+    awk '/start-vm/,/end-vm/ {if ($0 !~ /-vm--/) {print $0}}' | 
+    sed '2d; s/^| //; s/ |$//; s/  *|/|/g; s/|  */|/g' | 
+    sqlite3 -cmd ".mode markdown" -cmd ".import /dev/stdin labs" -cmd ".mode tabs" \
+    -cmd "select case when (select $vm from labs where Number = '$LAB_NUMBER') == '' then 'absent' else 'present' end"
+}
+
+bigip1=$(vm_present bigip1)
+bigip2=$(vm_present bigip2)
+echo bigip1: $bigip1, bigip2: $bigip2
+
 # confirm networking is up -- legacy from Skytap
 until ping -c 1 localhost; do sleep 1; done
 
