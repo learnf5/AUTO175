1b0bae5cd53cc2f899d602d6445bd53602294593
learnf5 <99698691+learnf5@users.noreply.github.com>
2025-07-31 14:13:32 -0400

    Update 10-AUTO175.sh

--- 10-AUTO175.sh
+++ 10-AUTO175.sh
@@ -18,18 +18,16 @@ function vm_status() {
 }
 
 function vm_status2() {
-  vm=$1
-
   # sqlite3 has absolutely terrible markdown parsing, hence the sed line noise ;-)
   echo -n $(curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/README.md | 
     awk '/start-vm-table/,/end-vm-table/ {if ($0 !~ /-vm-table--/) {print $0}}' | 
     sed '2d; s/^| //; s/ |$//; s/  *|/|/g; s/|  */|/g' | 
     sqlite3 -cmd ".mode markdown" -cmd ".import /dev/stdin labs" -cmd ".mode tabs" \
-    -cmd "SELECT CASE WHEN (SELECT $vm FROM labs WHERE Number = '$LAB_NUMBER') == '' THEN 'absent' ELSE 'present' END")
+    -cmd "SELECT CASE WHEN (SELECT $1 FROM labs WHERE Number = '$LAB_NUMBER') == '' THEN 'absent' ELSE 'present' END")
 }
 
-echo bigip1a: $(vm_status2 bigip1a)
-echo bigip1b: $(vm_status2 bigip1b)
+echo bigip1a: ===$(vm_status2 bigip1a)===
+echo bigip1b: ===$(vm_status2 bigip1b)===
 
 # confirm networking is up -- legacy from Skytap
 until ping -c 1 localhost; do sleep 1; done
